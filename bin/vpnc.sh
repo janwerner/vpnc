faab76edc47bfe74a74a4b20bcd59a584c67729d
janwerner <mail@janwerner.de>
2017-01-16 11:09:31 +0100

    Update vpnc.sh

--- bin/vpnc.sh
+++ bin/vpnc.sh
@@ -3,6 +3,7 @@
 cat > /etc/vpnc/default.conf <<EOF
 IPSec gateway ${VPNC_GATEWAY}
 IPSec ID ${VPNC_ID}
+IKE Authmode ${VPNC_AUTHMODE}
 IPSec secret ${VPNC_SECRET}
 Xauth username ${VPNC_USERNAME}
 Xauth password ${VPNC_PASSWORD}
